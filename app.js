19d30fcc153ccc15f756afdbe62fae403f800e9a
jacobedawson <reviber8@gmail.com>
2014-03-21 10:00:26 +0100

    Scroll Change
    Modified scroll, added the number of saved messages to 50

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/', function(req, res){
 
 io.sockets.on('connection', function(socket){
 	var query = Chat.find({});
-	query.sort('-created').limit(8).exec(function(err, docs) {
+	query.sort('-created').limit(50).exec(function(err, docs) {
 		if(err) throw err;
 		socket.emit('load old msgs', docs);
 	});

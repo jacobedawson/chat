31354e420ec287a3e43b73ba0ac71e1baace5913
jacobedawson <reviber8@gmail.com>
2014-04-10 01:07:55 +0200

    Clarified user name
    Added naming system to make it easy to understand who's chatting to whom

--- app.js
+++ app.js
@@ -64,19 +64,19 @@ io.sockets.on('connection', function(socket){
 			if(msg.substring(0,7) === 'http://' && msg.substring(msg.length - 4) === '.gif') {
 			    msg = '<img src=\"' + msg + '\"\/>';
 			} 
-
-			if (msg.substring(0,7) === 'http://') {
+			//checks for links, adds anchor tags
+			if (msg.substring(0,7) === 'http://' || msg.substring(0,8) === 'https://') {
 				msg = '<a href=\"' + msg + '\" target=\"_blank\">' + msg + '</a>';
 			}
-			//deal with secret messages
-
+			
+				//deal with secret messages
 				var name = pm;
-
+				//prevent people messaging themselves UPDATE: (no longer needed actually...)
 				if(users[name] === users[socket.nickname]) {
 					users[socket.nickname].emit('error', {msg: "lol u just tried 2 message urself haha get some sleep yo ;)", nick: socket.nickname});
 					return;
 				}
-
+				//prevent sending empty messages
 				if(name in users) {
 					if(msg.length == 0) { 
 						return; 
